4a6dcabeacb81bae4db0156d0d562d467cc2533f
Lisbeth Guerrero <lis@MacBook-Air-de-Lisbeth.local>
2020-03-05 17:46:31 -0300

    cambian metodo a delete

--- src/main/java/hackernews/example/controller/HackerNewsController.java
+++ src/main/java/hackernews/example/controller/HackerNewsController.java
@@ -3,6 +3,7 @@ package hackernews.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,7 +24,7 @@ public class HackerNewsController {
 	public @ResponseBody List<News> getAllNews() {
 		return service.getAllNews();
 	}
-	@GetMapping("/delete")
+	@DeleteMapping("/delete")
 	public @ResponseBody boolean delete(@RequestParam(required = true, value = "id") int id) {
 		return service.deleteNews(id);
 	}
