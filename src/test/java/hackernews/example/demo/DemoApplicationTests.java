8b1f491975cc84edfdad71a0b03a59b6b18bc6d7
Lisbeth Guerrero <lis@MacBook-Air-de-Lisbeth.local>
2020-03-05 21:16:32 -0300

    se elimina test innecesario

--- src/test/java/hackernews/example/demo/DemoApplicationTests.java
+++ src/test/java/hackernews/example/demo/DemoApplicationTests.java
@@ -1,8 +1,5 @@
 package hackernews.example.demo;
 
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,9 +9,5 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-	@Test
-	void date() {
-		System.out.println(OffsetDateTime.parse ( "2020-03-05T14:16:44.000Z" , DateTimeFormatter.ofPattern ( "yyyy-MM-dd HH:mm:ss.SSSZ" ) ) );
-	}
-	
+
 }
